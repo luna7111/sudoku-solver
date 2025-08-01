680de195c8b5b889f3d322e532800d44de1777e9
luna <luna7111@tuta.io>
2025-08-01 21:05:15 +0200

    Added start grid and print grid functions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
 use std::io;
+use color_print::cprint;
 
-/* struct Tile { */
-/*     number: u8, */
-/* } */
+#[derive(Clone)]
+struct Tile {
+    is_set: bool,
+    number: usize,
+}
 
-/* fn print_grid() { */
+struct Board {
+    size: usize,
+    region_size: usize,
+    grid: Vec<Vec<Tile>>,
+}
 
-/* } */
+fn is_perfect_square(number: usize) -> bool {
+    let sqrt = f32::sqrt(number as f32).floor();
+    return sqrt * sqrt == number as f32;
+}
 
-fn is_perfect_square(number: u8) -> bool {
-    let sqrt = f32::sqrt(number.into()).floor();
-    return sqrt * sqrt == number.into();
+fn print_grid(board: Board) {
+    for (row_index, row) in board.grid.iter().enumerate() {
+        for (tile_index, tile) in row.iter().enumerate() {
+            let color = if (row_index / board.region_size + tile_index / board.region_size) % 2 == 0 {"\u{1b}[34m"} else {"\u{1b}[33m"};
+            if tile.is_set == true {
+                cprint!("{}{}", color,  tile.number);
+            }
+            else {
+                cprint!("{}#", color);
+            }
+        }
+        println!("");
+    }
+}
+
+fn start_grid(size: usize) {
+
+    let mut board = Board {
+        size: size,
+        region_size: f32::sqrt(size as f32).floor() as usize,
+        grid: vec![vec![Tile { is_set: false, number: 0 }; size]; size],
+    };
+
+   print_grid(board);
 }
 
 fn main() {
@@ -20,13 +51,14 @@ fn main() {
     io::stdin().read_line(&mut buffer).expect("Failed to read from stdin");
 
     let trimmed = buffer.trim();
-    match trimmed.parse::<u8>() {
+    match trimmed.parse::<usize>() {
         Ok(size) => {
             if is_perfect_square(size) {
                 println!("Size is {}.", size);
+                start_grid(size);
             }
             else {
-                println!("Grid needs to be a perfect square (4, 9, 16...)")f
+                println!("Grid needs to be a perfect square (4, 9, 16...)");
             }
         },
         Err(..) => println!("Input is not an integer: {}.", trimmed),
